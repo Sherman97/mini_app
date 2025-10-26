86913e89fc402279de65e94e272d23740948c2b7
Sherman97 <grmanrjas@gmail.com>
2025-10-26 16:34:13 -0500

    fix, ajuste capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.german.ionicdemo',
   appName: 'ionic-todo',
   webDir: 'www'
 };
